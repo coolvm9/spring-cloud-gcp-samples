7ee544085c661fae9c23ffe4f3e91a8ec1422830
coolvm9 <coolvm9@gmail.com>
2023-11-23 08:26:49 -0800

    first commit

---
+++ src/test/java/com/fusion/gcp/sample/SpringCloudGcpSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusion.gcp.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGcpSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
